266b08679ee5cc1f9ea61c61faf91a8aaee51b49
706com <ehdgusjdbswl@gmail.com>
2024-01-04 23:27:04 +0900

    feat : 뷰 템플릿 띄우기

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-
     @GetMapping("/hi")
     public String niceToMeetYou(){
         return "greetings";
