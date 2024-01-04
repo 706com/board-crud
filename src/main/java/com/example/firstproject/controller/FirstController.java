01068e4ec76a57752962c6e3ab39a8406ce7dfcb
706com <ehdgusjdbswl@gmail.com>
2024-01-04 23:34:03 +0900

    feat : Model 생성 및 변수적용

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","Donghyun"); //변수명 , 변수값
         return "greetings";
     }
 }
