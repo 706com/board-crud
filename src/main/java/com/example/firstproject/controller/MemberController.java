801f82f8e351466f7ba3e3316d9f9dd017140643
706com <ehdgusjdbswl@gmail.com>
2024-01-11 12:58:14 +0900

    feat : 엔티티 조회 api 생성

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,10 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -31,5 +35,19 @@ public class MemberController {
         Member member = memberRepository.save(memberEntity);
         log.info(member.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member",member);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        List<Member> memberList = (ArrayList<Member>)memberRepository.findAll();
+        model.addAttribute("memberList",memberList);
+        return "members/index";
     }
 }
