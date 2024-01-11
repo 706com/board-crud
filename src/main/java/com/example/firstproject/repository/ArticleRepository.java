afc2de36558d047353da089abc4ada03ac64b686
706com <ehdgusjdbswl@gmail.com>
2024-01-11 12:45:03 +0900

    refactor : 반환값 오버라이딩(재정의)

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -1,7 +1,10 @@
 package com.example.firstproject.repository;
 
 import com.example.firstproject.entity.Article;
+import java.util.ArrayList;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleRepository extends CrudRepository<Article,Long> {
+    @Override
+    ArrayList<Article> findAll();   // Iterable -> ArrayList 반환값 오버라이딩
 }
