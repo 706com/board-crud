b1d334b48b95b45166fa2653d23d3e45110f25d4
706com <ehdgusjdbswl@gmail.com>
2024-01-21 04:33:09 +0900

    add : id 생성전략 추가

--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -3,6 +3,7 @@ package com.example.firstproject.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
