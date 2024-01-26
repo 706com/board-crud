ce65e29c96f091835b5b4a16eac219e28350a713
706com <ehdgusjdbswl@gmail.com>
2024-01-26 19:50:20 +0900

    feat : 댓글 생성 서비스 구현

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -31,9 +32,22 @@ public class CommentService {
 //            CommentDto dto = CommentDto.createCommentDto(c);
 //            dtos.add(dto);
 //        }
-        // 3. 결과 반환
+//        // 3. 결과 반환
         return commentRepository.findByArticleId(articleId).stream()
                 .map(comment -> CommentDto.createCommentDto(comment)) // map(a->b) : 스트림의 각 요소(a)를 꺼내 b를 수행한 결과로 매핑
                 .collect(Collectors.toList());  // 스트림 데이터를 리스트 자료형으로 변환
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! 대상 게시글이 없습니다."));
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto,article);
+        // 3. 댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+        // 4. DTO 변환 후 반환
+        return CommentDto.createCommentDto(created);
     }
 }
