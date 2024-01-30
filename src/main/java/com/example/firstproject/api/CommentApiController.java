f8e699337eaa0d20693ac3921dc8ccb8df595a70
706com <ehdgusjdbswl@gmail.com>
2024-01-30 18:00:00 +0900

    feat : 댓글 삭제 api 구현

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,4 +45,9 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
     // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public  ResponseEntity<CommentDto> delete(@PathVariable Long id){
+        CommentDto deletedDto = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+    }
 }
